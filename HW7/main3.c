d631bce8d903b699fc436ed1933609a055d35e7f
AndeTin <popolalaxxd@gmail.com>
2022-12-29 16:08:09 +0800

    main3

--- HW7/main3.c
+++ HW7/main3.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    for(int i=1; i<=10; i++){
-        int num;
-		printf("%d * %d = %d\n", num, i, num*i);
-	}
+    int i=0,a=1,b=1;
+    for(i=0;i<81;i++)
+    {
+        a=i/9+1;
+        b=i%9+1;
+        printf("%d*%d=%d\t ",a,b,a*b);
+        if(b==9)
+            printf("\n");
+    }
     return 0;
 }
