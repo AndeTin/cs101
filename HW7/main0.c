99baa07d686edfcfa7647dae5d982d6a594af4e5
AndeTin <popolalaxxd@gmail.com>
2022-12-29 11:37:55 +0800

    Update main0.c

--- HW7/main0.c
+++ HW7/main0.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 int main()
 {
+    int i=7,a,b;
+    for(a=1;a<=i;a++)
+    {
+        printf(" ");
+        for(b=1;b<=i-a;b++)
+        {
+            printf(" ");
+        }
+        for(b=1;b<=a;b++)
+        {
+            printf("%d ",a);
+        }
+        for(b=1;b<=a;b++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
