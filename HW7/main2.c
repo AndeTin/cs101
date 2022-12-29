7c4896353f61e1cb8d7510d04bb0a22cdf28e6a0
AndeTin <popolalaxxd@gmail.com>
2022-12-29 12:05:42 +0800

    3

--- HW7/main2.c
+++ HW7/main2.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 int main()
 {
+    double Pi=4.0;
+    int flag=1;
+    for(int i=3;i<=100000;i+=2)
+    {
+        if(flag==1)
+        {
+            Pi=Pi-(4.0/i);
+            flag=0;
+        }
+        else
+        {
+            Pi=Pi+(4.0/i);
+            flag=1;
+        }
+    }
+    printf("Pi=%f",Pi);
     
 }
