9b5374477586a394422c94381b35c21283c8434f
AndeTin <popolalaxxd@gmail.com>
2023-01-03 12:28:50 +0800

    Update main3.c

--- HW#8/main3.c
+++ HW#8/main3.c
@@ -1,30 +1,9 @@
 #include<stdio.h>
 
-// void get_binary(int i){
-//     int index=0;
-//     if(i!=0){
-//         if(i%2==1){
-//             if(index % 4 == 0){
-//                 printf(" ");
-//             }
-//             printf("1");
-//             index++;
-//             get_binary(i/2);
-//         }
-//         else{
-//             if(index % 4 == 0){
-//                 printf(" ");
-//             }
-//             printf("0");
-//             index++;
-//             get_binary(i/2);
-//         }
-//     }
-// }
-int index = 0;
+int index=0;
 void get_binary(int i){
-    if(i != 0){
-        if(i % 2 == 1){
+    if(i!=0){
+        if(i%2==1){
             printf("1");
             index++;
             if(index % 4 == 0){
@@ -44,6 +23,6 @@ void get_binary(int i){
 }
 
 int main(){
-    int i=129;
+    int i=128;
     get_binary(i);
 }
