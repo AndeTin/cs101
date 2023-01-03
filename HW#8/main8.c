1f645ebd9709d2839365655b9ff535c50be85eb5
AndeTin <popolalaxxd@gmail.com>
2023-01-04 01:13:21 +0800

    main8

---
+++ HW#8/main8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void print_spaces(int r)
+{
+    int i;
+    for(i=0;i<r;i++)
+    {
+        printf(" ");
+    }
+}
+void print_stars(int r)
+{
+    int i;
+    for(i=0;i<2*r-1;i++)
+    {
+        printf("*");
+    }
+}
+int main()
+{
+    int r,rows=5;
+    for(r=1;r<rows;r++)
+    {
+        print_spaces(r);
+        print_stars(rows-r);
+        printf("\n");
+    }
+    for(r=2;r<rows;r++){
+        print_spaces(rows-r);
+        print_stars(r);
+        printf("\n");
+    }
+}
