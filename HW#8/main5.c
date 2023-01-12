47439b30fe86f0447bfd5ac0c13963b0438bfb02
AndeTin <popolalaxxd@gmail.com>
2023-01-12 10:34:25 +0800

    main5

--- HW#8/main5.c
+++ HW#8/main5.c
@@ -1,54 +1,46 @@
 #include<stdio.h>
+
 void print_top(){
     printf("X'max tree!\n");
 }
 
 void print_spaces(int rows){
-    for(int i=0; i<rows; i++){
+    for(int i=0;i<rows;i++){
         printf(" ");
     }
 }
 
-
 void print_stars(int r){
-    for(int i=0; i< 2*r-1; i++){
+    for(int i=0;i<2*r-1;i++){
         printf("*");
     }
     printf("\n");
 }
 
 void print_trunk(int rows){
-    for(int i=0; i<rows; i++){
-        printf("       *\n");
-    }
+    printf("      *\n");
 }
 
 int main(){
-    int r=0,rows=8,index=0;
-    for(int i=0; i< rows; i++){
-        if(index==4){
-            continue;
-        }
-        print_spaces(rows-r);
-        print_stars(r);
-        r++;
-        index++;
-    }
-    
-    for(int i=0,r=0,index=0; i< rows; i++){
-        if(index==6){
-            continue;
-        }
-        print_spaces(rows-r);
-        print_stars(r);
-        r++;
-        index++;
-    }
-    for(int i=0,r=0; i< rows; i++){
+    int rows=7;
+    print_top();
+    for(int index=0,r=1;index<3;index++){
         print_spaces(rows-r);
         print_stars(r);
         r++;
     }
-    print_trunk(rows);
+    for(int index=0,r=1;index<5;index++){
+        print_spaces(rows-r);
+        print_stars(r);
+        r++;
+    }
+    for(int index=0,r=1;index<7;index++){
+        print_spaces(rows-r);
+        print_stars(r);
+        r++;
+    }
+    for(int index=0;index<5;index++){
+        print_trunk(rows);
+    }
 
 }
